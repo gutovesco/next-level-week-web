891d60e9874b3cb1d75601cfdf2d8b3bcb83b1ee
gutovesco <guto.vesco@hotmail.com>
2020-06-06 19:47:13 -0300

    global css

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import './App.css'
+import {Home} from './pages/Home'
 
 function App() {
   return (
     <div className="App">
+      <h1>Oiiioioioioioioii0</h1>
+      <Home/>
     </div>
   );
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './styles.css'
+
+export const Home = () => {
+    return (
+        <div>
+          <strong>Laaalallalalalalala</strong>  
+        </div>
+    )
+}
