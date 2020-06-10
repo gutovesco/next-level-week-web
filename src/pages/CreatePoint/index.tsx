047906e5942f13181e32960ec306cffbfa457345
gutovesco <guto.vesco@hotmail.com>
2020-06-10 12:14:31 -0300

    finishing map functionalities

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -6,12 +6,24 @@ import { FiArrowLeft } from 'react-icons/fi'
 import { Map, TileLayer, Marker } from 'react-leaflet'
 import api from '../../services/api'
 import axios from 'axios'
+import {LeafletMouseEvent} from 'leaflet'
 
 const CreatePoint = () => {
     const [items, setItems] = useState([])
     const [ufs, setUfs] = useState([])
     const [cidades, setCidades] = useState([])
     const [selectedUF, setSelectedUF] = useState('0')
+    const [selectedCity, setSelectedCity] = useState('0')
+    const [selectedPosition, setselectedPosition] = useState<[number, number]>([0, 0])
+    const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0])
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(position => {
+            const {latitude, longitude } = position.coords;
+
+            setInitialPosition([latitude, longitude])
+        })
+    }, [])
 
     useEffect(() => {
         api.get('/items').then(res => {
@@ -32,6 +44,13 @@ const CreatePoint = () => {
             setCidades(cities)
         })
     }, [selectedUF])
+
+    const handleMapClick = (e: LeafletMouseEvent) => {
+        setselectedPosition([
+            e.latlng.lat,
+            e.latlng.lng
+        ])
+    }
     
     return (
         <div id="page-create-point">
@@ -69,12 +88,12 @@ const CreatePoint = () => {
                     </div>
                 </fieldset>
 
-                <Map center={[-22.4111243, -46.9707956]} zoom={15}>
+                <Map center={initialPosition} zoom={15} onClick={handleMapClick}>
                     <TileLayer
                         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
-                    <Marker position={[-22.4111243, -46.9707956]}/>
+                    <Marker position={selectedPosition}/>
                 </Map>
 
                 <fieldset>
@@ -86,7 +105,7 @@ const CreatePoint = () => {
                     <div className="field-group">
                         <div className="field">
                             <label htmlFor="uf">Estado (UF)</label>
-                            <select  onChange={(e: any) => setSelectedUF(e.target.value)}  name="uf" id="uf">
+                            <select  onChange={(e: any) => setSelectedUF(e.target.value)} value={selectedUF}  name="uf" id="uf">
                                 <option value="0">Selecione uma UF</option>
                                 {ufs.map(uf => (
                                     <option key={uf} value={uf}>{uf}</option>
@@ -95,7 +114,7 @@ const CreatePoint = () => {
                         </div>
                         <div className="field">
                             <label htmlFor="cidade">cidade</label>
-                            <select name="cidade" id="cidade">
+                            <select onChange={(e: any) => setSelectedCity(e.target.value)} value={selectedCity} name="cidade" id="cidade">
                                 <option value="0">Selecione uma cidade</option>
                                 {cidades.map(city => (
                                     <option key={city} value={city}>{city}</option>
